3cc84f7c0ab068b9b34fdd90e05d6dd209d2d0a4
Anna Datskiv <93anna.kr@gmail.com>
2021-10-21 01:04:12 +0300

    created coroutines

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,66 +1,77 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.lang.IllegalArgumentException
 import java.util.*
+import kotlin.jvm.Throws
 
-sealed class Calc (val a:Int, val b:Int) {
-    class Plus(a: Int, b: Int) : Calc(a,b)
-    class Minus(a: Int, b: Int) : Calc(a,b)
-    class Divide(a: Int, b: Int) : Calc(a,b)
-    class Multiply(a: Int, b: Int) : Calc(a,b)
+sealed class Calc(val a: Int, val b: Int) {
+    class Plus(a: Int, b: Int) : Calc(a, b)
+    class Minus(a: Int, b: Int) : Calc(a, b)
+    class Divide(a: Int, b: Int) : Calc(a, b)
+    class Multiply(a: Int, b: Int) : Calc(a, b)
 }
 
-data class Result(val operation:Calc, val result:Float)
+data class Result(val operation: Calc, val result: Float)
 
-fun calculate(list: List<Calc>,resultCallBack:(List<Result>)->Unit){
+suspend fun calculate(list: List<Calc>, resultCallBack: (List<Result>) -> Unit) {
     val resultList = ArrayList<Result>()
     list.forEach {
-        when(it){
-            is Calc.Divide -> {
-                if (it.a in 1..10 && it.b in 1..10){
-                val c=it.a.toFloat()/it.b
-                val result = Result(it,c)
-                resultList.add(result)
-            }
-                else
-                    throw IllegalArgumentException("Значения должны быть в пределах от 1 до 10")
-            }
+        withContext(Dispatchers.IO) {
+            when (it) {
+                is Calc.Divide -> {
+                    try {
+                        checkRange(it.a, it.b)
+                        val c = it.a.toFloat() / it.b
+                        val result = Result(it, c)
+                        resultList.add(result)
+                    } catch (e: IllegalArgumentException) {
+                        println(e.message.toString())
+                    }
+                }
 
-            is Calc.Minus -> {
-                if (it.a in 1..10 && it.b in 1..10){
-                val c=it.a-it.b
-                val result = Result(it,c.toFloat())
-                resultList.add(result)
-            }
-                else
-                    throw IllegalArgumentException("Значения должны быть в пределах от 1 до 10")
-            }
-
-
-            is Calc.Multiply -> {
-                    if (it.a in 1..10 && it.b in 1..10){
-                val c=it.a*it.b
-                val result = Result(it,c.toFloat())
-                resultList.add(result)
-            }
-                    else
-                        throw IllegalArgumentException("Значения должны быть в пределах от 1 до 10")
-            }
+                is Calc.Minus -> {
+                    try {
+                        checkRange(it.a, it.b)
+                        val c = it.a - it.b
+                        val result = Result(it, c.toFloat())
+                        resultList.add(result)
+                    } catch (e: IllegalArgumentException) {
+                        println(e.message.toString())
+                    }
+                }
 
 
-            is Calc.Plus -> {
-                    if (it.a in 1..10 && it.b in 1..10){
-                val c=it.a+it.b
-                val result = Result(it,c.toFloat())
-                resultList.add(result)
-            }
-               else
-                    throw IllegalArgumentException("Значения должны быть в пределах от 1 до 10")
+                is Calc.Multiply -> {
+                    try {
+                        checkRange(it.a, it.b)
+                        val c = it.a * it.b
+                        val result = Result(it, c.toFloat())
+                        resultList.add(result)
+                    } catch (e: IllegalArgumentException) {
+                        println(e.message.toString())
+                    }
+                }
+
+
+                is Calc.Plus -> {
+                    try {
+                        checkRange(it.a, it.b)
+                        val c = it.a + it.b
+                        val result = Result(it, c.toFloat())
+                        resultList.add(result)
+                    } catch (e: IllegalArgumentException) {
+                        println(e.message.toString())
+                    }
+                }
             }
         }
-
-
     }
     resultCallBack(resultList)
-
 }
 
-
+@Throws(IllegalArgumentException::class)
+fun checkRange(a: Int, b: Int) {
+    if (a !in 1..10 || b !in 1..10) {
+        throw IllegalArgumentException("Значения должны быть в пределах от 1 до 10")
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,26 @@
 import kotlinx.coroutines.*
-import java.util.*
 import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
+
+
+    val inputList = ArrayList<Calc>()
+    val myPlus = Calc.Plus(0, 5)
+    val myMinus = Calc.Minus(4, 9)
+    val myDivide = Calc.Divide(6, 1)
+    val myMultiply = Calc.Multiply(4, 7)
+
+    inputList.add(myPlus)
+    inputList.add(myMinus)
+    inputList.add(myDivide)
+    inputList.add(myMultiply)
+
     runBlocking {
+        calculate(inputList) { resultList ->
+            println(resultList.toString())
         }
+    }
+}
 
-        val inputList = ArrayList<Calc>()
-            val myPlus = Calc.Plus(3, 5)
-            val myMinus = Calc.Minus(4, 9)
-            val myDivide = Calc.Divide(6, 1)
-            val myMultiply = Calc.Multiply(4, 7)
-
-            inputList.add(myPlus)
-            inputList.add(myMinus)
-            inputList.add(myDivide)
-            inputList.add(myMultiply)
-
-
-            try {
-                calculate(inputList) { resultList ->
-                    println(resultList.toString())
-                }
-            } catch (e: IllegalArgumentException) {
-                println(e.message.toString())
-            }
-        }
 
 
